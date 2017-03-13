cae09449a8f392405f82cf8ee4901da11a0ed030
pennyheadaway <jombi@web.de>
2017-03-13 16:31:11 +0100

    ADD: born

---
+++ multi_plot.py
@@ -0,0 +1,41 @@
+import math
+import itertools
+import matplotlib.pyplot as plt
+
+# plotting helper functions
+def plot_images(images, label=None, n_cols=5, cmap=None):
+    """
+    plot images in n_cols columns
+    :param images
+    :param label image labels
+    :param n_cols number of columns
+    """
+    n_rows = math.ceil(len(images)/n_cols)
+
+    plt.figure(figsize=(20, n_rows*10/n_cols))
+    for i, image in enumerate(images):
+        #image = image.squeeze()
+        plt.subplot(n_rows, n_cols, i+1)
+        if label != None:
+            plt.title("Output " + str(label[i]))
+        plt.imshow(image, aspect='equal', cmap=cmap)
+    #plt.savefig(output_folder + "pipeline")
+    plt.show()
+
+def plot_compare_images(left_images, right_images, cmap=None):
+    """
+    compare two image list side by side
+    """
+    assert len(left_images) == len(right_images)
+    mixed_images = list(itertools.chain.from_iterable(zip(left_images, right_images)))
+    plot_images(mixed_images, n_cols=2, cmap=cmap)
+
+def plot_histogram(labels, n_labels):
+    """
+    Exploration of the label distribution
+    """
+    plt.hist(labels, n_labels)
+    plt.xlabel('Labels')
+    plt.ylabel('Label Count')
+    plt.title('Histogram')
+    plt.show()
